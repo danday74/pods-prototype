249e3346dc304adac1ca374ca5b16f854d97be6f
Daniel Lewis <daniellewis777@gmail.com>
2023-08-01 17:05:05 +0100

    create pod done

---
+++ src/app/modules/create-pod-modal/components/create-pod-modal/create-pod-modal.component.ts
@@ -0,0 +1,81 @@
+import { Component, EventEmitter, Output } from '@angular/core'
+import { IPod } from '../../../../interfaces/i-pod'
+import { kebabCase } from 'lodash-es'
+import { getRandomNumber } from '../../../../utils/number-utils'
+
+@Component({
+  selector: 'app-create-pod-modal',
+  templateUrl: './create-pod-modal.component.html',
+  styleUrls: ['./create-pod-modal.component.scss']
+})
+export class CreatePodModalComponent {
+
+  go = false
+  stats = false
+  pod: IPod = {
+    id: null,
+    text: null,
+    number: null,
+    buttonText: 'View',
+    dark: false,
+    addedByHuman: true
+  }
+
+  @Output() cancel = new EventEmitter<void>()
+  @Output() create = new EventEmitter<IPod>()
+
+  cancelCreatePod() {
+    this.cancel.emit()
+  }
+
+  createPod() {
+    if (this.pod.text && this.pod.text.trim().length) {
+      const text: string = this.pod.text.trim()
+      this.pod = {
+        ...this.pod,
+        id: kebabCase(text),
+        text,
+        number: this.stats ? getRandomNumber(99) : null
+      }
+      this.create.emit(this.pod)
+    }
+  }
+
+  toggleViewGo() {
+    this.go = !this.go
+    this.pod.buttonText = this.go ? 'Go' : 'View'
+  }
+
+  toggleStats() {
+    this.stats = !this.stats
+    this.pod.number = this.stats ? 99 : null
+  }
+
+  clickSun() {
+    this.pod.dark = false
+  }
+
+  clickMoon() {
+    this.pod.dark = true
+  }
+
+  clickView() {
+    this.go = false
+    this.pod.buttonText = 'View'
+  }
+
+  clickGo() {
+    this.go = true
+    this.pod.buttonText = 'Go'
+  }
+
+  clickStatsOff() {
+    this.stats = false
+    this.pod.number = null
+  }
+
+  clickStatsOn() {
+    this.stats = true
+    this.pod.number = 99
+  }
+}
