a65d6a755ce114d6fabf0637141d438411bba507
Daniel Lewis <daniellewis777@gmail.com>
2023-08-01 01:41:53 +0100

    ripple

--- src/app/modules/pod/pod.module.ts
+++ src/app/modules/pod/pod.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { PodComponent } from './components/pod/pod.component'
+import { MatRippleModule } from '@angular/material/core'
 
 @NgModule({
     declarations: [
@@ -9,8 +10,9 @@ import { PodComponent } from './components/pod/pod.component'
     exports: [
         PodComponent
     ],
-    imports: [
-        CommonModule
-    ]
+  imports: [
+    CommonModule,
+    MatRippleModule
+  ]
 })
 export class PodModule {}
